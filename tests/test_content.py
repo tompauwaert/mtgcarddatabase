c7314ff2fe9e1b00e850045f150e0cac81c73a05
Tom Pauwaert <tom.pauwaert@gmail.com>
2015-07-14 14:25:20 +0200

    Test refactoring - making different suites Fixed a few tests

---
+++ tests/test_content.py
@@ -0,0 +1,105 @@
+import unittest
+import json
+
+import mock
+import requests.exceptions
+import mtgdb.content_provider
+import mtgdb.exceptions
+import tests.data.mtgjson_testdata as mtgjson_data
+
+
+class ContentAvailabilityTest(unittest.TestCase):
+
+    def setUp(self):
+        self.availability = mtgdb.content_provider.ContentAvailability()
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent.available_sets',
+                return_value=mtgjson_data.data_sets())
+    def test_shouldReturnListOfAllOfficialyAvailableSets(self):
+        self.assertListEqual(self.availability.available_sets(), mtgjson_data.data_sets())
+
+
+class MtgjsonContentTest(unittest.TestCase):
+
+    def setUp(self):
+        self.content = mtgdb.content_provider.MtgjsonContent()
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local', return_value=None)
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote')
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._save_data_local')
+    def test_shouldReturnListOfAllAvailableSetsFromInternetLast(self, m_save, m_remote,
+                                                                m_local):
+        # mock returns zipped test-data json.
+        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data)
+
+        sets = self.content.available_sets()
+        self.assertListEqual(sets, mtgjson_data.data_sets(),
+                             "Test returns incorrect list of available sets.")
+        self.assertTrue(m_remote.called)
+        self.assertTrue(m_save.called_with_args(self.content._ID_ALLSETS_X,
+                                                json.loads(mtgjson_data.data)))
+
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local',
+                return_value=mtgjson_data.data_file(mtgjson_data.data))
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote')
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._save_data_local')
+    def test_shouldReturnListOfAllAvailableSetsFromLocalStorageFirst(self, m_local,
+                                                                     m_remote, m_save) :
+        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data)
+
+        # Fake existence of the data on local storage so that
+        # no requests to the internet are made if the file is present.
+        # Mock out the open() function.
+        sets = self.content.available_sets()
+
+        self.assertListEqual(sets, mtgjson_data.data_sets(),
+                             "Test returns incorrect list of available sets.")
+        # at least one call to open must have tried reading the set data locally
+        self.assertTrue(m_local.called)
+        self.assertFalse(m_remote.called, 'should not attempt to request from internet.')
+        self.assertFalse(m_save.called, 'should not attempt to save to local storage, it already exists')
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local', return_value=None)
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote')
+    def test_shouldReturnExceptionIfTheJsonCannotBeReadFromInternet(self, m_remote, m_local):
+        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data_malformed_json)
+        self.assertRaises(mtgdb.exceptions.InvalidDataError, self.content.available_sets)
+
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local', return_value=None)
+    @mock.patch('mtgdb.content_provider.requests.get')
+    def test_shouldReturnExceptionIfTheZipCannotBeReadFromInternet(self, m_req, m_local):
+        m_req.return_value = mock.MagicMock()
+        m_req.return_value.content = mtgjson_data.faulty_zipped()
+        self.assertRaises(mtgdb.exceptions.InvalidDataError, self.content.available_sets)
+
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local', return_value=None)
+    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote',
+                side_effect=requests.exceptions.ConnectionError)
+    def test_shouldReturnExceptionIfCantAccessInternet(self, m_remote, m_local):
+        self.assertRaises(requests.exceptions.ConnectionError, self.content.available_sets)
+
+def load_tests(loader, standard_tests, pattern):
+    test_cases = [
+        MtgjsonContentTest,
+        ContentAvailabilityTest,
+    ]
+    suite = unittest.TestSuite()
+    for test_class in test_cases:
+        tests = loader.loadTestsFromTestCase(test_class)
+        suite.addTests(tests)
+    return suite
+
+# def suite():
+#     test_classes = [
+#         MtgjsonContentTest,
+#         ContentAvailabilityTest,
+#     ]
+#     suite = unittest.TestSuite()
+#     for test_class in test_classes:
+#         suite.addTests(unittest.TestLoader().loadTestsFromTestCase(test_class))
+#     return suite
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,93 +1,18 @@
 import unittest
-import mock
-import requests.exceptions
-import json
-import StringIO
 
-import mtgdb.content_provider
-import mtgdb.exceptions
+# Test files
+test_modules = [
+    'tests.test_content',
+]
+suite = unittest.TestSuite()
+for module in test_modules:
+    try:
+        mod = __import__(module, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
 
-import tests.data.mtgjson_testdata as mtgjson_data
+    except (ImportError, AttributeError):
+        # else just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(module))
 
-
-class ContentAvailabilityTest(unittest.TestCase):
-
-    def setUp(self):
-        self.availability = mtgdb.content_provider.ContentAvailability()
-
-    def test_shouldReturnListOfAllOfficialyAvailableSets(self):
-        pass
-
-
-class MtgjsonContentTest(unittest.TestCase):
-
-    def setUp(self):
-        self.content = mtgdb.content_provider.MtgjsonContent()
-
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local', return_value=None)
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote')
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._save_data_local')
-    def test_shouldReturnListOfAllAvailableSetsFromInternetLast(self, m_save, m_remote, m_local):
-        # mock returns zipped test-data json.
-        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data)
-
-        sets = self.content.available_sets()
-        self.assertListEqual(sets, mtgjson_data.data_sets(),
-                             "Test returns incorrect list of available sets.")
-        self.assertTrue(m_remote.called)
-        self.assertTrue(m_save.called_with_args(self.content._ID_ALLSETS_X,
-                                                json.loads(mtgjson_data.data)))
-
-
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_local',
-                return_value=mtgjson_data.data_file(mtgjson_data.data))
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._get_data_remote')
-    @mock.patch('mtgdb.content_provider.MtgjsonContent._save_data_local')
-    def test_shouldReturnListOfAllAvailableSetsFromLocalStorageFirst(self, m_local, m_remote, m_save) :
-        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data)
-
-        # Fake existence of the data on local storage so that
-        # no requests to the internet are made if the file is present.
-        # Mock out the open() function.
-        sets = self.content.available_sets()
-
-        self.assertListEqual(sets, mtgjson_data.data_sets(),
-                             "Test returns incorrect list of available sets.")
-        # at least one call to open must have tried reading the set data locally
-        self.assertTrue(m_local.called)
-        self.assertFalse(m_remote.called, 'should not attempt to request from internet.')
-        self.assertFalse(m_save.called, 'should not attempt to save to local storage, it already exists')
-
-    @mock.patch('mtgdb.content_provider.Mtgjsoncontent._get_data_local', return_value=None)
-    @mock.patch('mtgdb.content_provider.Mtgjsoncontent._get_data_remote')
-    def test_shouldReturnExceptionIfTheJsonCannotBeReadFromInternet(self, m_remote, m_local):
-        m_remote.return_value = mtgjson_data.data_file(mtgjson_data.data_malformed_json)
-        self.assertRaises(mtgdb.exceptions.InvalidDataError, self.content.available_sets)
-
-
-    @mock.patch('mtgdb.content_provider.Mtgjsoncontent._get_data_local', return_value=None)
-    @mock.patch('mtgdb.content_provider.requests.get')
-    def test_shouldReturnExceptionIfTheZipCannotBeReadFromInternet(self, m_req, m_local):
-        m_req.return_value = mock.MagicMock()
-        m_req.return_value.content = mtgjson_data.faulty_zipped()
-        self.assertRaises(mtgdb.exceptions.InvalidDataError, self.content.available_sets)
-
-    @mock.patch('mtgdb.content_provider.Mtgjsoncontent._get_data_local', return_value=None)
-    @mock.patch('mtgdb.content_provider.Mtgjsoncontent._get_data_remote',
-                side_effect=requests.exceptions.ConnectionError)
-    def test_shouldReturnExceptionIfCantAccessInternet(self, m_local):
-        self.assertRaises(requests.exceptions.ConnectionError, self.content.available_sets)
-
-def load_tests(loader, standard_tests, pattern):
-    test_cases = [
-        MtgjsonContentTest,
-    ]
-    suite = unittest.TestSuite()
-    for test_class in test_cases:
-        tests = loader.loadTestsFromTestCase(test_class)
-        suite.addTests(tests)
-    return suite
-
-
-if __name__ == "__main__":
-    unittest.main()
+unittest.TextTestRunner(verbosity=2).run(suite)
